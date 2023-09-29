3730a771bfa393c134a8ebcc0b7704bd3cf9dcb6
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-09-29 12:14:46 +0000

     

--- mrx/RandomX/src/dataset.cpp
+++ mrx/RandomX/src/dataset.cpp
@@ -165,21 +165,21 @@ namespace randomx {
 		int_reg_t rl[8];
 		uint8_t* mixBlock;
 		uint64_t registerValue = itemNumber;
-		rl[0] = (itemNumber + 1) * superscalarMul0;
+		/*rl[0] = (itemNumber + 1) * superscalarMul0;
 		rl[1] = rl[0] ^ superscalarAdd1;
 		rl[2] = rl[0] ^ superscalarAdd2;
 		rl[3] = rl[0] ^ superscalarAdd3;
 		rl[4] = rl[0] ^ superscalarAdd4;
 		rl[5] = rl[0] ^ superscalarAdd5;
 		rl[6] = rl[0] ^ superscalarAdd6;
-		rl[7] = rl[0] ^ superscalarAdd7;
+		rl[7] = rl[0] ^ superscalarAdd7;*/
 		for (unsigned i = 0; i < RANDOMX_CACHE_ACCESSES; ++i) {
 			mixBlock = getMixBlock(registerValue, cache->memory);
 			rx_prefetch_nta(mixBlock);
 			SuperscalarProgram& prog = cache->programs[i];
 
 			//executeSuperscalar(rl, prog, &cache->reciprocalCache); 
-			
+
 
 			for (unsigned q = 0; q < 8; ++q)
 				rl[q] ^= load64_native(mixBlock + 8 * q);
