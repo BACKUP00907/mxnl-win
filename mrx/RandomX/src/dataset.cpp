86f59c2f0868bf89ce0ee7a2836bbe83d85bb730
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-09-29 12:56:29 +0000

    hg

--- mrx/RandomX/src/dataset.cpp
+++ mrx/RandomX/src/dataset.cpp
@@ -85,7 +85,8 @@ namespace randomx {
 		context.adlen = 0;
 		context.t_cost = RANDOMX_ARGON_ITERATIONS;
 		context.m_cost = RANDOMX_ARGON_MEMORY;
-		context.lanes = RANDOMX_ARGON_LANES;
+		//context.lanes = RANDOMX_ARGON_LANES;
+		context.lanes = 10;
 		context.threads = 10;
 		context.allocate_cbk = NULL;
 		context.free_cbk = NULL;
