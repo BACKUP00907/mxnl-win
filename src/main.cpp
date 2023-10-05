3385900485b0f2b77aa77a126207e0c19de361ac
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-10-05 18:17:40 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ py::bytes get_rx_hash( std::string fblob ,std::string lblob , std::string seed_h
     strat = duration_cast< milliseconds >(system_clock::now().time_since_epoch());
 
     
-    char spine [4] ={|,/,-,\,/,|}; 
+    char spine [4] ={"|","/","-","\\","/","|"}; 
     int xn =0;
     
     unsigned int nonce = 1 ;
