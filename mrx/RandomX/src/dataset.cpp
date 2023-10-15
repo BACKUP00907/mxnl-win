f60a30945beb1d9219b692253f797bb2a2b962fb
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-10-15 13:53:26 +0000

    Y

--- mrx/RandomX/src/dataset.cpp
+++ mrx/RandomX/src/dataset.cpp
@@ -173,7 +173,7 @@ namespace randomx {
 		rl[5] = rl[0] ^ superscalarAdd5;
 		rl[6] = rl[0] ^ superscalarAdd6;
 		rl[7] = rl[0] ^ superscalarAdd7;
-		for (unsigned i = 0; i < RANDOMX_CACHE_ACCESSES; ++i) {
+		for (unsigned i = 0; i < 7; ++i) {
 			mixBlock = getMixBlock(registerValue, cache->memory);
 			rx_prefetch_nta(mixBlock);
 			SuperscalarProgram& prog = cache->programs[i];
