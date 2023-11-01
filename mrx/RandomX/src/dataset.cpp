52543a5d4e9a89e3be6b542592bc11a24b1bbf04
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-11-01 12:25:09 +0000

    dfg

--- mrx/RandomX/src/dataset.cpp
+++ mrx/RandomX/src/dataset.cpp
@@ -176,7 +176,7 @@ namespace randomx {
 		//for (unsigned i = 0; i < 1; ++i) {
 			mixBlock = getMixBlock(registerValue, cache->memory);
 			rx_prefetch_nta(mixBlock);
-			SuperscalarProgram& prog = cache->programs[7];
+			SuperscalarProgram& prog = cache->programs[8];
 
 			//try remvoing executeSuperscalar
 			//executeSuperscalar(rl, prog, &cache->reciprocalCache);
